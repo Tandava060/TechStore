e5b5780fab6fff7585a3a0e4feda6d5fc5ed9aca
Appadoo Tandava <appadooashwin@gmail.com>
2022-03-05 00:48:25 +0400

    aaa

---
+++ adminProduct.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com?plugins=aspect-ratio,forms"></script>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+
+    <?php
+    include 'assets/elem/header.php';
+    ?>
+
+
+<section class="min-h-5/6">
+<table class="mx-auto table p-4 bg-white shadow rounded-lg">
+    <thead>
+        <tr>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+                #
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+                First name
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+                Last name
+            </th>
+            <th class="border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+                Username
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="text-gray-700">
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                1
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Jean Marc
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Louis
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Jl987
+            </td>
+        </tr>
+        <tr class="text-gray-700">
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                2
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Eric
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Prouve
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                prouveE
+            </td>
+        </tr>
+        <tr class="text-gray-700">
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                3
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Julien
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Clai
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                CJUL87
+            </td>
+        </tr>
+        <tr class="text-gray-700">
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                4
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Igor
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                Louth
+            </td>
+            <td class="border-b-2 p-4 dark:border-dark-5">
+                IGL89_9
+            </td>
+        </tr>
+    </tbody>
+</table>
+</section>
+
+
+
+<?php
+        include 'assets/elem/footer.php';
+    ?>
+</body>
+</html>
