8a58f715c7291488a5a48ae5881183af8f21d7cb
Appadoo Tandava <appadooashwin@gmail.com>
2022-03-04 22:27:50 +0400

    bug

--- assets/elem/header.php
+++ assets/elem/header.php
@@ -329,6 +329,5 @@ if(isset($_SESSION["uid"])){
         </div>
     </header>
 </section>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <script src="assets/js/sidecart.js"></script>
